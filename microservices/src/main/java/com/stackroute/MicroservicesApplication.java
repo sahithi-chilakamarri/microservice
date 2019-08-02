1d13ef8cc5f1cf4513117f975f9811384d7c146a
nagapremika <sadhupremika@gmail.com>
2019-08-02 09:42:56 +0530

    Commiting Eureka server

---
+++ microservices/src/main/java/com/stackroute/MicroservicesApplication.java
@@ -0,0 +1,19 @@
+package com.stackroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+
+public class MicroservicesApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MicroservicesApplication.class, args);
+	}
+
+}
